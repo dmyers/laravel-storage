ede6e7a1baa7504b8681bd399ba67d4afd853a78
Derek Myers <arcticpro@gmail.com>
2014-05-06 02:16:43 -0500

    Changed local route to use new storage render method

--- src/routes.php
+++ src/routes.php
@@ -4,16 +4,10 @@ if ($route_path = Storage::config('route_path')) {
 	// Register storage route.
 	\Route::get($route_path.'{path}', function ($path) {
 		try {
-			$file = Storage::get($path);
+			$response = Storage::render($path);
 		} catch (\Exception $e) {
 			\App::abort(404, 'File not found');
 		}
-		
-		$mime = Storage::mime($path);
-		
-		$response = \Response::make($file, 200, array(
-			'Content-Type' => $mime,
-		));
 		
 		return $response;
 	});
